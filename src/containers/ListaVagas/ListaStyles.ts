d898949694e7efefeccfb511ce505f220701b57e
Alex <alex_soares_oliveira@hotmail.com>
2024-08-05 10:42:21 -0300

    atualizando a lista

---
+++ src/containers/ListaVagas/ListaStyles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ListaVagasContainer = styled.div`
+  padding: 20px;
+`
+
+export const VagasList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`
+
+export const VagaItem = styled.li`
+  margin-bottom: 16px;
+`
